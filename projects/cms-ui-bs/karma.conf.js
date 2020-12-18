c9672204a9cfd111c7a925ce978fea591e29ed32
Pham Anh Tuan <tuanpa2295@gmail.com>
2020-12-18 10:29:51 +0700

    Rename project

--- projects/cms-ui-bs/karma.conf.js
+++ projects/cms-ui-bs/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ng-main'),
+      dir: require('path').join(__dirname, '../../coverage/cms-ui-bs'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- projects/cms-ui-bs/src/app/app.component.spec.ts
+++ projects/cms-ui-bs/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-main'`, () => {
+  it(`should have as title 'cms-ui-bs'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-main');
+    expect(app.title).toEqual('cms-ui-bs');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-main app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('cms-ui-bs app is running!');
   });
 });
